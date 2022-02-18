b8c6f394969600462491a4ec7204b6a92805816d
Victor Calebe <victorcalebesilva10@gmail.com>
2022-02-17 21:29:55 -0300

    Day-3

--- javaScript-programandoPOO/ContaCorrente.js
+++ javaScript-programandoPOO/ContaCorrente.js
@@ -1,12 +1,34 @@
+import {Cliente} from "./Cliente.js"
 export class ContaCorrente{
+    static numContas = 0;
     agencia;
-    cliente;
+    _cliente;
 
-    #saldo = 0;
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this._cliente = cliente;
+        ContaCorrente.numContas +=1;
+    }
+
+    set cliente(novoCliente){
+        if (novoCliente instanceof Cliente) {
+            this._cliente = novoCliente;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
-        if (this.#saldo >= valor) {
-            this.#saldo = this.#saldo - valor;
+        if (this._saldo >= valor) {
+            this._saldo = this._saldo - valor;
         }else{
             console.log("Não á saldo suficiente");
         }
@@ -14,7 +36,7 @@ export class ContaCorrente{
 
     depositar(valor){
         if(valor>0){
-            this.#saldo = this.#saldo + valor;
+            this._saldo = this._saldo + valor;
         }else{
             console.log("Não é possivel depositar valores menores que 0");
         }
@@ -30,7 +52,8 @@ export class ContaCorrente{
     }
 
     exibirConta(){
-        console.log("Cliente: "+this.cliente.nome,this.cliente.cpf+"\nAgencia: "+this.agencia+" | ","Saldo: "+this.#saldo);
-        return this.#saldo;
+        console.log("Cliente: "+this._cliente.nome+"\nAgencia: "+this.agencia+" | ","Saldo: "+this._saldo);
+        return this._saldo;
     }
+
 }
--- javaScript-programandoPOO/index.js
+++ javaScript-programandoPOO/index.js
@@ -1,27 +1,12 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-//cliente 1
-const cliente1 = new Cliente();
-cliente1.nome = "victor";
-cliente1.cpf = "50350350340";
 
-const conta1 = new ContaCorrente();
-conta1.agencia = "500";
-conta1.cliente = cliente1;
+const cliente1 = new Cliente("victor",404040404040);
 
-//cliente 2
-const cliente2 = new Cliente();
-cliente2.nome = "calebe";
-cliente2.cpf = "50350330340";
+const conta1 = new ContaCorrente(505,cliente1);
+conta1.depositar(300);
 
-const conta2 = new ContaCorrente();
-conta2.agencia = "500";
-conta2.cliente = cliente2;
-
-//execucao
-conta1.depositar(1000);
-conta1.sacar(900);
-conta1.transferir(50,conta2);
-
+conta1.exibirConta();
+console.log(ContaCorrente.numContas);
 
