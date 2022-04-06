8cb2d681a71df9be4e3aec469207535d5675c474
Victor Calebe <victorcalebesilva10@gmail.com>
2022-04-05 21:53:34 -0300

    Dia 12 Interagindo com propriedades, e interação entre classes

--- react-entendoOFuncionamentoLib/ceep/src/App.js
+++ react-entendoOFuncionamentoLib/ceep/src/App.js
@@ -5,11 +5,23 @@ import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.notas = [];
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    this.notas.push(novaNota);
+    console.log(this.notas.length)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
